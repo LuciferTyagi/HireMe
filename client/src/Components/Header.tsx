cc12346727bb03f65517a035e2bcea5fb491bc27
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2025-02-06 21:56:26 +0530

    Header

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,9 +1,17 @@
-const Header = () => {
-  return (
-    <header className="w-full border-b h-16">
-      
-    </header>
-  )
+const Header: React.FC = () => {
+    return (
+        <header className="w-full border-b">
+                 <nav className="max-w-[1440px] mx-auto bg-red-300 py-4 flex justify-between px-28 font-clash tracking-tighter text-3xl font-medium mt-24">
+                        <span className="">Yagyansh Tyagi.</span>
+                        <ul className="flex gap-11">
+                             <li><a href="/" className="">Home</a></li>
+                             <li><a href="/" className="">About</a></li>
+                             <li><a href="/" className="">Works</a></li>
+                             <li><a href="/" className="">Contact</a></li>
+                        </ul>
+                 </nav>
+        </header>
+    )
 }
 
 export default Header
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        clash: ['ClashDisplay-Regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
