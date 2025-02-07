3c253285f89d4ca98b86234f14bcd6730d292935
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2025-02-07 13:20:43 +0530

    Mobile Header

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,7 +1,13 @@
+import { faMoon, faX } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons/faBars"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+
 const Header: React.FC = () => {
+const[showMobileMenu , setShowMobileMneu] = useState(false);
     return (
         <header className="w-full border-b">
-                 <nav className="max-w-[1440px] mx-auto bg-red-300 py-4 flex justify-between px-28 font-clash tracking-tighter text-3xl font-medium mt-24">
+                 <nav className="Desktop-Navbar hidden lg:flex max-w-[1440px] mx-auto bg-red-300 py-4  justify-between px-28 font-clash tracking-tighter text-3xl font-medium mt-24">
                         <span className="">Yagyansh Tyagi.</span>
                         <ul className="flex gap-11">
                              <li><a href="/" className="">Home</a></li>
@@ -9,6 +15,30 @@ const Header: React.FC = () => {
                              <li><a href="/" className="">Works</a></li>
                              <li><a href="/" className="">Contact</a></li>
                         </ul>
+                 </nav>
+
+                 <nav className="Mobile-Navbar lg:hidden w-full mx-auto bg-red-300 py-2 flex justify-between items-center px-4 font-clash tracking-tighter  font-medium mt-12">
+                       <span className="text-2xl">Yagyansh Tyagi.</span>
+                       <button onClick={() => setShowMobileMneu(true)} className="flex items-center"><FontAwesomeIcon icon={faBars} className="size-5"/></button>
+                       {showMobileMenu && (
+                           <div className="absolute inset-0 w-full h-screen bg-white px-4">
+                                 <div className="Tools flex justify-between items-center mt-6">
+                                      <span className="text-2xl">Yagyansh Tyagi.</span>
+                                      <div className="flex gap-4">
+                                            <button><FontAwesomeIcon icon={faMoon} className="size-5"/></button>
+                                            <button onClick={() => setShowMobileMneu(false)}><FontAwesomeIcon icon={faX} className="size-5"/></button>
+                                      </div>
+                                 </div>
+                                 
+                                 <ul className="flex flex-col gap-1 text-[26px] mt-4 ml-3">
+                                     <li><a href="/">Home</a></li>
+                                     <li><a href="/">About</a></li>
+                                     <li><a href="/">Works</a></li>
+                                     <li><a href="/">Contact</a></li>
+                                    
+                                 </ul>
+                           </div>
+                       )}
                  </nav>
         </header>
     )
